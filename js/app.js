1546d736dbfab7bbe77e59fc88ec96f8f0e473ef
carolina <carolinaccez@gmail.com>
2020-07-16 21:36:22 -0400

    2 working api functions

--- js/app.js
+++ js/app.js
@@ -4,12 +4,71 @@ $(document).foundation() //do not change this line
 
 
 //Action handlers and listeners
-$('#titleBtn').on('click', test);
+$('#titleBtn').on('click', firstApiCall);
 
-function test(){
-    console.log('success');
-    console.log($('#movieTitle').val());
+function test() {
+	console.log('success');
+	console.log($('#movieTitle').val());
 }
+
+function firstApiCall(event) {
+	event.preventDefault()
+	let ApiValueOne = $("#movieTitle").val().trim()
+
+	let firstUrlApi = "https://movie-database-imdb-alternative.p.rapidapi.com/?page=1&r=json&s=" + ApiValueOne
+
+	$.ajax({
+		url: firstUrlApi,
+		type: "GET",
+		headers: {
+			"x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com",
+			"x-rapidapi-key": "ea27b482e3mshcea35e8094dc17ep18e197jsn4219678d9404"
+		}
+	})
+		.then(function (response) {
+			console.log(response)
+			secondApiCall(response.Search[0].imdbID);
+
+		})
+}
+
+//function returns multiple movies, should we display more examples spider-man
+function secondApiCall(ID) {
+	console.log(ID);
+
+	let secondUrlApi = "https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/" + ID 
+	console.log(secondUrlApi);
+
+	$.ajax({
+		url: secondUrlApi,
+		type: "GET",
+		dataType: "json",
+		headers: {
+			"x-rapidapi-host": "imdb-internet-movie-database-unofficial.p.rapidapi.com",
+			"x-rapidapi-key": "ea27b482e3mshcea35e8094dc17ep18e197jsn4219678d9404"
+		}
+	})
+		.then(function (response) {
+			console.log(response);
+
+		})
+
+}
+
+// function thirdApiCall() {
+// 	let ApiValueThird = $("").val() 
+
+//     let thirdUrlApi = ""
+
+//     $.ajax({
+//         url: thirdUrlApi , 
+// 		type: "GET" , 
+// 		headers : {}
+//     })
+//     .then(function(response){
+
+// 	}) 
+// }
 
 /*
 Movie Database. use this to search by text and get the imdbID (also returns poster & title)
