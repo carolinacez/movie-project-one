31f8d403ef80cbd8bf04b098275bce1537a6fb75
carolina <carolinaccez@gmail.com>
2020-07-18 13:28:16 -0400

    error handling, fixed third api call and getItem from localStorage in second api call

--- js/vendor/details.js
+++ js/vendor/details.js
@@ -1,46 +1,58 @@
 $(document).foundation() //do not change this line
 
-function secondApiCall(ID) {
-	console.log(ID);
+function secondApiCall() {
+    let ID = localStorage.getItem("movieId");
+    console.log(ID);
 
-	let secondUrlApi = "https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/" + ID 
-	console.log(secondUrlApi);
+    let secondUrlApi = "https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/" + ID
+    console.log(secondUrlApi);
 
-	$.ajax({
-		url: secondUrlApi,
-		type: "GET",
-		dataType: "json",
-		headers: {
-			"x-rapidapi-host": "imdb-internet-movie-database-unofficial.p.rapidapi.com",
-			"x-rapidapi-key": "ea27b482e3mshcea35e8094dc17ep18e197jsn4219678d9404"
-		}
-	})
-		.then(function (response) {
+    $.ajax({
+        url: secondUrlApi,
+        type: "GET",
+        dataType: "json",
+        headers: {
+            "x-rapidapi-host": "imdb-internet-movie-database-unofficial.p.rapidapi.com",
+            "x-rapidapi-key": "ea27b482e3mshcea35e8094dc17ep18e197jsn4219678d9404"
+        }
+    })
+        .then(function (response) {
+            console.log(response)
             //display function goes here 
-			console.log(response);
-
-		})
-
+        })
 }
 
 function thirdApiCall() {
-	
+    let ID = localStorage.getItem("movieId");
 
-    let thirdUrlApi = "https://streamzui-streamzui-v1.p.rapidapi.com/search?type=Movie&country=us&page=1"
+    let thirdUrlApi = "https://streamzui-streamzui-v1.p.rapidapi.com/search?type=Movie&imdbid=" + ID +
+        "&country=us&page=1"
 
     $.ajax({
-		url: thirdUrlApi ,
-		"async" : true,
-	"crossDomain" : true, 
-		type: "GET" , 
-		dataType: "json" ,
-		headers : {"x-rapidapi-host": "streamzui-streamzui-v1.p.rapidapi.com",
-		"x-rapidapi-key": "ea27b482e3mshcea35e8094dc17ep18e197jsn4219678d9404"}
+        url: thirdUrlApi,
+        "async": true,
+        "crossDomain": true,
+        type: "GET",
+        dataType: "json",
+        headers: {
+            "x-rapidapi-host": "streamzui-streamzui-v1.p.rapidapi.com",
+            "x-rapidapi-key": "ea27b482e3mshcea35e8094dc17ep18e197jsn4219678d9404"
+        }
     })
-    .then(function(response){
-        //display function goes here 
-		console.log(response);
+        .then(function (response) {
+            if (response.Count >= 1) {
 
-	}) 
+                //display function goes here                 
+                console.log(response);
+            } else {
+                //do nothing or state no Amazon Prime available 
+                console.log("No Amazon Prime")
+            }
+        })
 }
 
+function onLoad() {
+    secondApiCall();
+    thirdApiCall();
+}
+onLoad();
