86f76cfcfd8c05a9ff4fa69c1ee0caa08ad3484e
Khargol <josephklein3@gmail.com>
2020-07-22 16:15:42 -0400

    trailer plays with youtube

--- js/vendor/details.js
+++ js/vendor/details.js
@@ -20,6 +20,7 @@ function secondApiCall() {
             //console.log(response)
             //display function goes here 
             display(response);
+            displayTrailer(response);
         })
 }
 
@@ -54,7 +55,7 @@ function thirdApiCall() {
 
 function onLoad() {
     secondApiCall();
-    thirdApiCall();
+    //thirdApiCall();
 }
 
 window.addEventListener("DOMContentLoaded" , onLoad);
@@ -99,12 +100,12 @@ function display(data){
     runTime.innerHTML = data.length;
     titleEl.appendChild(runTime);
 
-    let link = document.createTextNode('Link to Trailer');
-    let trailerLink  = document.createElement('a');
-    trailerLink.appendChild(link);
-    trailerLink.href = data.trailer.link;
-    trailerLink.title = "Link to trailer."
-    trailerEl.appendChild(trailerLink);
+    // let link = document.createTextNode('Link to Trailer');
+    // let trailerLink  = document.createElement('a');
+    // trailerLink.appendChild(link);
+    // trailerLink.href = data.trailer.link;
+    // trailerLink.title = "Link to trailer."
+    // trailerEl.appendChild(trailerLink);
     
     
     
@@ -112,3 +113,62 @@ function display(data){
     
 }
 
+function displayTrailer(data){
+    console.log(data);
+    let searchString = makeYTId(data.title);
+
+    let secondUrlApi = "https://youtube-search1.p.rapidapi.com/" + searchString
+    console.log(secondUrlApi);
+
+    $.ajax({
+        url: secondUrlApi,
+        type: "GET",
+        dataType: "json",
+        headers: {
+            "x-rapidapi-host": "youtube-search1.p.rapidapi.com",
+            "x-rapidapi-key": "ea27b482e3mshcea35e8094dc17ep18e197jsn4219678d9404"
+        }
+    })
+        .then(function (response) {
+            //console.log(response)
+            //display function goes here 
+            makePlayer(response);
+          
+        })
+}
+
+
+
+function makeYTId (movieTitle){
+    console.log(movieTitle);
+    let tempTitle = movieTitle.trim().split(" ");
+    let searchTitle = "";
+    console.log(tempTitle);
+  
+    for(var i = 0; i < tempTitle.length - 1; i++){
+        searchTitle = searchTitle + tempTitle[i] + '+';
+    }
+
+    searchTitle = searchTitle + tempTitle[tempTitle.length -1] + "official+movie+trailer";
+    console.log(searchTitle);
+    return searchTitle;
+}
+
+function makePlayer (data){
+    let video = data.items[0].id.videoId;
+    console.log(video);
+    let url = "https://www.youtube.com/embed/" + video;
+
+    let playerEl = document.getElementById('trailer');
+    let ifrm = document.createElement('iframe');
+    ifrm.setAttribute('width', '560');
+    ifrm.setAttribute('height', '315');
+    ifrm.setAttribute('id', 'ifrm');
+    ifrm.setAttribute('src', url);
+    ifrm.setAttribute('allow', 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture');
+    ifrm.setAttribute('allowfullscreen', '');
+
+    playerEl.appendChild(ifrm);
+
+
+}
